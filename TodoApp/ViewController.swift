9e5a4d9b98452a44206caed86c349c546cc6eed8
Ecem <ecembostancioglu21@gmail.com>
2024-01-16 14:27:30 +0300

    TableView and configureItems function were created

--- TodoApp/ViewController.swift
+++ TodoApp/ViewController.swift
@@ -6,14 +6,39 @@
 //
 
 import UIKit
+import CoreData
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
 
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    
+        tableView.dataSource = self
+        tableView.delegate = self
+        
+        configureItems()
+    }
+    
+    private func configureItems(){
+        navigationController?.navigationBar.topItem?.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.add, target: self, action: #selector(buttonClicked))
     }
 
-
+    @IBAction func buttonClicked(_ sender: Any) {
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = "Hello"
+        return cell
+        
+    }
+    
 }
 
