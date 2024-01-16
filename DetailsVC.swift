9818cfba7e3ed54c00336175341c40621d49ba3b
Ecem <ecembostancioglu21@gmail.com>
2024-01-16 16:40:14 +0300

    DetailsVC and segue were created

---
+++ DetailsVC.swift
@@ -0,0 +1,8 @@
+//
+//  DetailsVC.swift
+//  TodoApp
+//
+//  Created by Ecem Bostancıoğlu on 16.01.2024.
+//
+
+import Foundation
---
+++ TodoApp/DetailsVC.swift
@@ -0,0 +1,29 @@
+//
+//  DetailsVC.swift
+//  TodoApp
+//
+//  Created by Ecem Bostancıoğlu on 16.01.2024.
+//
+
+import UIKit
+
+class DetailsVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
