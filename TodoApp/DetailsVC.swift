ba7914a16ba0b1100e90cc39aca4c4089b3d71df
Ecem <ecembostancioglu21@gmail.com>
2024-01-16 17:24:29 +0300

    Values were saved

--- TodoApp/DetailsVC.swift
+++ TodoApp/DetailsVC.swift
@@ -6,16 +6,14 @@
 //
 
 import UIKit
+import CoreData
 
 class DetailsVC: UIViewController {
 
     
     @IBOutlet weak var todoField: UITextField!
-    
     @IBOutlet weak var descriptionField: UITextField!
-    
     @IBOutlet weak var deadlineDP: UIDatePicker!
-    
     @IBOutlet weak var imageView: UIImageView!
     
     
@@ -27,6 +25,23 @@ class DetailsVC: UIViewController {
     
     
     @IBAction func saveButtonClicked(_ sender: Any) {
+        
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context = appDelegate.persistentContainer.viewContext
+        let newTodo = NSEntityDescription.insertNewObject(forEntityName: "ToDoList", into: context)
+        
+        newTodo.setValue(todoField.text!, forKey: "todo")
+        newTodo.setValue(descriptionField.text!, forKey: "descript")
+        newTodo.setValue(deadlineDP.date, forKey: "date")
+        newTodo.setValue(UUID(), forKey: "id")
+        
+        do{
+            try context.save()
+            print("saved")
+        }catch{
+            print("error")
+        }
+        
     }
     
     
